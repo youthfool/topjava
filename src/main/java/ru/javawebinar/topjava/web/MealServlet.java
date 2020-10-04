27b8b20b0df05dcf20cffb13dc5b298e22e79d79
Sergey <klpkvgrey@mail.ru>
2020-10-04 17:39:12 +1000

    Done HW01

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,46 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class MealServlet extends HttpServlet {
+    private static final Logger log = getLogger(UserServlet.class);
+    private static final int CALORIES_PER_DAY = 2000;
+    private List<Meal> meals = new CopyOnWriteArrayList<>(Arrays.asList(
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
+            new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
+    ));
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm");
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.debug("forward to meals");
+        List<MealTo> mealTos = MealsUtil.filteredByStreams(meals, LocalTime.MIN, LocalTime.MAX, CALORIES_PER_DAY);
+        request.setAttribute("mealsList", mealTos);
+        request.setAttribute("dateTimeFormatter", formatter);
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,61 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: klpkv
+  Date: 04.10.2020
+  Time: 15:06
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html lang="ru">
+<head>
+    <title>Meals</title>
+    <style type="text/css">
+        table {
+            border-collapse: collapse;
+            margin-top: 10px;
+        }
+        th {
+            padding: 5px;
+            border: 2px solid black;
+        }
+    </style>
+</head>
+<body>
+<h3><a href="index.html">Home</a></h3>
+<hr>
+<h2>Meals</h2>
+<table>
+    <thead>
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+        <th></th>
+        <th></th>
+    </tr>
+    </thead>
+    <tbody>
+    <c:forEach var="m" items="${requestScope.mealsList}">
+        <tr style="color: <c:out value="${m.excess ? 'red' : 'green'}"/>">
+            <th>
+                <c:out value="${requestScope.dateTimeFormatter.format(m.dateTime)}"/>
+            </th>
+            <th>
+            <c:out value="${m.description}"/>
+            </th>
+            <th>
+                <c:out value="${m.calories}"/>
+            </th>
+            <th>
+                <a href="<c:out value="#"/>">Update</a>
+            </th>
+            <th>
+                <a href="<c:out value="#"/>">Delete</a>
+            </th>
+        </tr>
+    </c:forEach>
+    </tbody>
+</table>
+</body>
+</html>
