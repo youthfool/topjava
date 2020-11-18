944757750ead18b0b6201c8bdf4b2d6cc00fe092
Sergey <klpkvgrey@mail.ru>
2020-11-18 14:12:39 +1000

    done HW05 optional 1

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.web;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.util.StringUtils;
+import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.web.meal.MealRestController;
 
@@ -17,17 +18,21 @@ import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 
+import static ru.javawebinar.topjava.Profiles.REPOSITORY_IMPLEMENTATION;
 import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
 import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
 
 public class MealServlet extends HttpServlet {
 
-    private ConfigurableApplicationContext springContext;
+    private ClassPathXmlApplicationContext springContext;
     private MealRestController mealController;
 
     @Override
     public void init() {
-        springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml");
+        springContext = new ClassPathXmlApplicationContext();
+        springContext.getEnvironment().setActiveProfiles(Profiles.getActiveDbProfile(), REPOSITORY_IMPLEMENTATION);
+        springContext.setConfigLocations("spring/spring-app.xml", "spring/spring-db.xml");
+        springContext.refresh();
         mealController = springContext.getBean(MealRestController.class);
     }
 
--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -14,23 +14,30 @@ import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
+import static ru.javawebinar.topjava.Profiles.REPOSITORY_IMPLEMENTATION;
+
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 automatic resource management (ARM)
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/inmemory.xml")) {
-            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
-            System.out.println();
+        ClassPathXmlApplicationContext appCtx = new ClassPathXmlApplicationContext();
+        appCtx.getEnvironment().setActiveProfiles(Profiles.getActiveDbProfile(), REPOSITORY_IMPLEMENTATION);
+        appCtx.setConfigLocations("spring/spring-app.xml", "spring/spring-db.xml");
+        appCtx.refresh();
 
-            MealRestController mealController = appCtx.getBean(MealRestController.class);
-            List<MealTo> filteredMealsWithExcess =
-                    mealController.getBetween(
-                            LocalDate.of(2020, Month.JANUARY, 30), LocalTime.of(7, 0),
-                            LocalDate.of(2020, Month.JANUARY, 31), LocalTime.of(11, 0));
-            filteredMealsWithExcess.forEach(System.out::println);
-            System.out.println();
-            System.out.println(mealController.getBetween(null, null, null, null));
-        }
+        System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+        AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+        adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
+        System.out.println();
+
+        MealRestController mealController = appCtx.getBean(MealRestController.class);
+        List<MealTo> filteredMealsWithExcess =
+                mealController.getBetween(
+                        LocalDate.of(2020, Month.JANUARY, 30), LocalTime.of(7, 0),
+                        LocalDate.of(2020, Month.JANUARY, 31), LocalTime.of(11, 0));
+        filteredMealsWithExcess.forEach(System.out::println);
+        System.out.println();
+        System.out.println(mealController.getBetween(null, null, null, null));
+
+        appCtx.close();
     }
 }
